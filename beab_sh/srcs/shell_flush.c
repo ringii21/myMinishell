e3c7dbb2d07c69f7093f878587d57c457a35156b
ringii21 <anais.bonard@gmail.com>
2022-12-14 16:47:15 +0100

    Leaks free for basic commands

--- beab_sh/srcs/shell_flush.c
+++ beab_sh/srcs/shell_flush.c
@@ -6,34 +6,11 @@
 /*   By: abonard <abonard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:13:55 by seozcan           #+#    #+#             */
-/*   Updated: 2022/12/14 15:53:19 by abonard          ###   ########.fr       */
+/*   Updated: 2022/12/14 16:46:50 by abonard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-
-/* void	free_redir(t_redir *r)
-{
-	t_redir	*tmp;
-
-	while (r)
-	{
-		tmp = r->next;
-		if (r->file_path)
-			free(r->file_path);
-		if (r->file_name)
-			free(r->file_name);
-		free(r);
-		r = tmp;
-	}
-}
-
-void	free_token(t_token *t)
-{
-	if (t->bin_path)
-		free(t->bin_path);
-	ft_free_stab(t->cmds_av);
-} */
 
 void ft_free_nodes(t_token *t)
 {
@@ -61,21 +38,8 @@ void ft_free_nodes(t_token *t)
 
 void	ft_flush(t_token *t)
 {
-/* 	t_token	*next;
-
-	while (t && t->next)
-		t = t->prev;
-	while (t)
-	{
-		next = t->next;
-		free_token(t);
-		if (t->file)
-			free_redir(t->file);
-		t->cmd_ac = 0;
-		free(t);
-		t = next;
-	} */
 	t_token	*next;
+	
 	while (t && t->next)
 		t = t->prev;
 	while (t)
